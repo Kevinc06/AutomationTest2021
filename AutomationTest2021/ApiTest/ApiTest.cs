0f1471a27db7f4bbc652f14abf1cd2ede2e6d03d
kevinc06 <kevin_mallorca@outlook.com>
2021-03-19 16:08:13 -0300

    First Commit

---
+++ AutomationTest2021/ApiTest/ApiTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using System;
+using AutomationTest2021.Methods;
+using System.Net;
+
+namespace AutomationTest2021.ApiTest
+{
+    [TestFixture]
+    class ApiTest
+    {
+        [Test]
+        public void FirstTest()
+        {
+            string url = "https://reqres.in/api/users";
+            int expectedBehaviour = 201;
+            string content = "{\"Name\":\"Mickey Mouse\",\"Job\":\"CEO\"}";
+
+
+        }
+    }
+}
+
+
---
+++ AutomationTest2021/Methods/Methods.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+
+namespace AutomationTest2021.Methods
+{
+    public static class RestMethods
+    {
+
+    }
+}
+
